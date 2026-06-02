b7c262bdae6ce516d1e8c5c1b2f42e97371f84de
Justin Seaward <jut_seaward@yahoo.com>
2026-06-01 21:36:29 -0230

    created main file for single and double linked list classes

---
+++ src/main/java/LinkedList/DoubleMain.java
@@ -0,0 +1,15 @@
+package LinkedList;
+
+import DoubleLinkedList.DoubleLinkedList;
+
+public class DoubleMain {
+    public static void main(String[] args) {
+        DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+        doubleLinkedList.insertLinkedList(1,0);
+        doubleLinkedList.insertLinkedList(2,1);
+        doubleLinkedList.insertLinkedList(3,2);
+        doubleLinkedList.insertLinkedList(4,3);
+        doubleLinkedList.traverseLinkedList();
+        doubleLinkedList.reverseTraverseLinkedList();
+    }
+}
